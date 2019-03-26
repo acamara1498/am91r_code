ede2e5cba501d2a701469c7c8d12ed523b8d3e04
acamara1498 <acamara@college.harvard.edu>
2019-03-25 22:08:22 -0400

    log info

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
+    logger.info("Beginning to gather tweets")
     gst.getTweets("FB")
     logger.info("DONE gathering tweets for FB......")
     gst.getTweets("GOOG")
